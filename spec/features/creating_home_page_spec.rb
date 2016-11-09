98bacc65cc3636c9f84b16bee1774399e7ccdf7c
sam0191 <samer.abunuwara@gmail.com>
2016-11-09 22:41:12 +0200

    Add homepage

--- spec/features/creating_home_page_spec.rb
+++ spec/features/creating_home_page_spec.rb
@@ -5,7 +5,7 @@ RSpec.feature "Creating home page" do
 	scenario do
 
 		visit "/"
-		expect(page).to have_link("athletes Den")
+		expect(page).to have_link("Athletes Den")
 		expect(page).to have_link("Home")
 		expect(page).to have_content("Workout Lounge")
 	end
