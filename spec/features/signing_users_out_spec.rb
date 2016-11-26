3a99288c49d4d8ac7913600a0701de35713c86b3
sam0191 <samer.abunuwara@gmail.com>
2016-11-26 22:41:16 +0200

    Implemnet sign out

---
+++ spec/features/signing_users_out_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.feature "signing users out" do
+	before do 
+		@sam = User.create!(email: "sam@example.com", password: "111111")
+		visit "/"
+
+		click_link "Sign in"
+
+		fill_in "Email", with: @sam.email
+		fill_in "Password", with: @sam.password
+
+		click_button "Log in"
+	end
+	scenario "sign out" do
+		visit "/"		
+
+		click_link "Sign out"
+
+		expect(page).to have_content("Signed out successfully.")
+	end
+end
