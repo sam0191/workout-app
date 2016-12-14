26d683c8e084f92f537d3dccbb18e26b7c6d66f8
sam0191 <samer.abunuwara@gmail.com>
2016-12-14 18:22:10 +0200

    List members on home page

---
+++ spec/features/listing_members_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.feature "Listing Users" do 
+	before do 
+		@john = User.create(first_name: "john", last_name: "Doe", email: "johndoe@example.com", password: "password")
+		@sarah = User.create(first_name: "sarah", last_name: "joseph", email: "sarah@example.com", password: "password")
+	end
+
+	scenario "shows a list of registered members" do 
+		visit "/"
+		expect(page).to have_content("List of Members")
+		expect(page).to have_content(@john.full_name)
+		expect(page).to have_content(@sarah.full_name)
+	end
+end
